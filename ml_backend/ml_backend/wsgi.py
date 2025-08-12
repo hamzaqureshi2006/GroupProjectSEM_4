e716f664778687c8acf766bf3449e7332c70fe9a
hamzaqureshi2006 <mo.hamzaqureshi2006@gmail.com>
2025-08-12 14:36:59 +0530

    changed name of djangoproject

--- ml_backend/ml_backend/wsgi.py
+++ ml_backend/ml_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for comments project.
+WSGI config for ml_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comments.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ml_backend.settings')
 
 application = get_wsgi_application()
